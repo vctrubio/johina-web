2046fcb555abda4e28a3b3b816d8f8eea9b27178
vctrubio <vctrubio@gmail.com>
2024-08-09 13:22:47 +0200

    navbar change

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,29 @@
 import Link from 'next/link';
-import styles from './NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.row}>
-        <Link href="/murals" className={styles.link}>
-          Murals
-        </Link>
-        <Link href="/press" className={styles.link}>
-          Press
-        </Link>
-        <Link href="/workshops" className={styles.link}>
-          Workshops
-        </Link>
-      </div>
-      <div className={styles.row}>
-        <Link href="/contact" className={styles.link}>
-          Contact
-        </Link>
-        <Link href="/about" className={styles.link}>
-          About
-        </Link>
-      </div>
+    <nav >
+      <Link href="/murals" >
+        Murals
+      </Link>
+      <Link href="/press" >
+        Press
+      </Link>
+      <Link href="/about" >
+        About
+      </Link>
     </nav>
   );
 };
 
 export default NavBar;
+
+
+/*
+        <Link href="/contact" >
+          Contact
+        </Link>
+        <Link href="/workshops" >
+          Workshops
+        </Link>
+*/
