00c90bf4c430094fb6bcbe8c9238c9b82093ebdc
vctrubio <vctrubio@gmail.com>
2024-08-13 16:12:28 +0200

    mural page id

--- src/app/murals/[title]/page.js
+++ src/app/murals/[title]/page.js
@@ -1,6 +1,16 @@
 import { slugTitle, titleSlug } from "@/utils";
 import { fetchMuralById } from "@/lib/fetchQueries"
 
+const MuralIdPhotos = ({ photos }) => {
+    return (
+        <div className="mural-id-photos">
+            {photos.map((photo, index) => (
+                <img key={index} src={photo.url} alt={photo.title}  />
+            ))}
+        </div>
+    );
+}
+
 const MuralId = async ({ params }) => {
     let ptr;
 
@@ -12,15 +22,16 @@ const MuralId = async ({ params }) => {
     }
 
     return (
-        <div className="border">
+        <div className="border" style={{ margin: 'auto' }}>
             {ptr ? (
-                <>
-                    <h1>Title: {ptr.title}</h1>
-                    <p>Description: {ptr.description}</p>
-                    <p>Location: {ptr.location}</p>
-                    <p>Close Up: {ptr.closeUp}</p>
-                    <p>Photos Count: {ptr.photosCollection.items.length}</p>
-                </>
+                <div className="mural-id-container">
+                    <h1>{ptr.title}</h1>
+                    <MuralIdPhotos photos={ptr.photosCollection.items} />
+                    <div className="elements">
+                        <p>{ptr.description} asldjlaksdja asd f asdf asfd as dfasasldjlaksdja asd f asdf asfd as dfas dasldjlaksdja asd f asdf asfd as dfas dasldjlaksdja asd f asdf asfd as dfas dasldjlaksdja asd f asdf asfd as dfas dasldjlaksdja asd f asdf asfd as dfas dasldjlaksdja asd f asdf asfd as dfas d d asldjlaksdja asd f asdf asfd as dfas d</p>
+                        <p style={{minWidth: '140px', height: '42px', textAlign: 'right', border: '1px solid black', paddingRight: '2px'}}>Location: {ptr.location}</p>
+                    </div>
+                </div>
             ) : <h1>Not found page</h1>}
         </div>
     );
