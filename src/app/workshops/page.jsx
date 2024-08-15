1e554df259c949c078291d05b755e5873de9c345
vctrubio <vctrubio@gmail.com>
2024-08-15 10:19:31 +0200

    workshop sticky scrolling

--- src/app/workshops/page.jsx
+++ src/app/workshops/page.jsx
@@ -1,7 +1,20 @@
 const Helloworld = () => {
-    return ( <>
-
-    </> );
+    return (
+        <div className="sticky-scroll-container">
+            <div className="sticky-scroll">
+                    <h1>Workshop One</h1>
+                    <p>Some context</p>
+            </div>
+            <div className="sticky-scroll">
+                    <h1>Workshop Two</h1>
+                    <p>Some content</p>
+            </div>
+            <div className="sticky-scroll">
+                    <h1>Workshop Three</h1>
+                    <p>Some content</p>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Helloworld;
