e81f506449d88cff4bf1ad28fe285f7f17088c1a
vctrubio <vctrubio@gmail.com>
2024-08-16 16:35:15 +0200

    instagram call good

--- components/Iphone.jsx
+++ components/Iphone.jsx
@@ -1,4 +1,16 @@
-const Iphone = () => {
+import React from 'react';
+
+const mediaId = 'your-media-id';
+const fields = 'id,caption,media_url,media_type,permalink';  // Replace this with the specific fields you want, e.g., 'id,caption,media_type,media_url'
+const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${process.env.INSTAGRAM_KEY}`;
+
+const Iphone = async() => {
+    const data = await fetch(url)
+    const media = await data.json();
+
+    console.log('checking')
+    console.log(media);
+
     return (
         <div className="iphone">
             <div className="screen">
