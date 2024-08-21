d723f5fbdc6722fbe0e47bc4a830e6c7feca9d0d
vctrubio <vctrubio@gmail.com>
2024-08-21 14:12:49 +0200

    image icon mas mas

--- components/ShareMe.jsx
+++ components/ShareMe.jsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import { WhatsappShareButton, WhatsappIcon, EmailShareButton, EmailIcon } from 'react-share';
-import Image from 'next/image';
+import { EmailShareButton, WhatsappShareButton, PinterestShareButton, TwitterShareButton, EmailIcon, WhatsappIcon, PinterestIcon, TwitterIcon } from 'react-share';import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 export const ShareComponent = () => {
@@ -14,7 +13,8 @@ export const ShareComponent = () => {
     setBtnToggle((prevState) => (prevState === 'off' ? 'on' : 'off'));
   };
 
-
+  const emailSubject = "Hi All! Check out this mural I found!";
+  const emailBody = "I found this mural and thought you might like it. Check it out here: "
   return (
     <>
       <div className="share-btn-container" btn-toggle={btnToggle} onClick={handleToggle}>
@@ -26,21 +26,32 @@ export const ShareComponent = () => {
         />
       </div>
       {btnToggle === 'on' && (
-      <div className="share-btns-z">
-        <div className='share-btns-link'>{path}</div>
-        <div className='share-btns-btns'>
-          <WhatsappShareButton url={path} separator=":: ">
-            <WhatsappIcon size={42} round />
-          </WhatsappShareButton>
-          <EmailShareButton url={path} separator=":: ">
-            <EmailIcon size={42} round />
-          </EmailShareButton>
+        <div className="share-btns-z">
+          <div className='share-btns-link'>{path}</div>
+          <div className='share-btns-btns'>
+            <EmailShareButton url={path} separator=".-> " subject={emailSubject} body={emailBody}>
+              <EmailIcon size={42} round />
+            </EmailShareButton>
+            <WhatsappShareButton url={path} title={emailSubject} separator='-> '>
+              <WhatsappIcon size={42} round />
+            </WhatsappShareButton>
+            <TwitterShareButton url={path} title={emailSubject}>
+              <TwitterIcon size={42} round />
+            </TwitterShareButton>
+            <PinterestShareButton url={path} media={`${domain}/path/to/image.jpg`} description={emailSubject}>
+              <PinterestIcon size={42} round />
+            </PinterestShareButton>
+          </div>
+          <div className='share-btns-close'>
+            <button onClick={handleToggle}>Close</button>
+          </div>
         </div>
-        <div className='share-btns-close'>
-          <button onClick={handleToggle}>Close</button>
-        </div>
-      </div>
-      )} 
+      )}
     </>
   );
 };
+
+
+/*
+  Good for now, pintinteres and twitter need to get the exact image path
+*/
