c08abb3f8a9e315e2544f44d2600f9608f63b67e
vctrubio <vctrubio@gmail.com>
2024-08-22 13:24:37 +0200

    modifying for migration

--- components/Instagraming.jsx
+++ components/Instagraming.jsx
@@ -9,7 +9,7 @@ export const InstagramCard = ({ media }) => {
                     alt={media.caption || 'Instagram src'}
                     width={600}
                     height={600}
-                    unoptimized //must be commented for dev
+                // unoptimized //must be commented for dev
                 />
                 <p className="instagram-caption">{media.caption}</p>
             </a>
@@ -17,61 +17,12 @@ export const InstagramCard = ({ media }) => {
     );
 };
 
-const sortInstagramCategory = async () => {
-    /* NOTE, THERE IS NO EDGES, ALBUMS ECT*/
-    const fields = 'id,caption,media_url,media_type,permalink';  // Replace this with the specific fields you want, e.g., 'id,caption,media_type,media_url'
-    const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${process.env.INSTAGRAM_KEY}`;
-    let media = null;
-
-    try{
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            console.error('Failed to fetch Instagram media:', res.status, res.statusText);
-            const errorResponse = await res.json();
-            console.error('Error details:', errorResponse);
-        }
-
-        media = await res.json();
-    }
-    catch(err){
-        console.error("🚀 ~ InstagramFeed ~ err:", err)
-    }
-    
-    const igMedia = []
-    const igAlbum = []
-    const igVideo = []
 
 
-    media.data.forEach(item => {
-        if (item.media_type === 'IMAGE') {
-            igMedia.push(item)
-        } else if (item.media_type === 'CAROUSEL_ALBUM') {
-            igAlbum.push(item)
-        } else if (item.media_type === 'VIDEO') {
-            igVideo.push(item)
-        }
-    })
-
-    return {
-        igMedia,
-        igAlbum,
-        igVideo
-    }
-    // const filteredMedia = media.data.filter(item => item.media_type === 'IMAGE').slice(0, 6);
-    // return filteredMedia
-}
-
-
-
-export const InstagramFeed = async () => {
-    /* NOTE, THERE IS NO EDGES, ALBUMS ECT*/
-
-    const { igMedia, igAlbum, igVideo } = await sortInstagramCategory()
-
+export const InstagramFeed = async ({content}) => {
     return (
         <div className='w-full'>
-            {igMedia.map((media, index) => (
+            {content.map((media, index) => (
                 <InstagramCard key={index} media={media} />
             ))}
         </div>
