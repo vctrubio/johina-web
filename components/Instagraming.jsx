748728e6091148e0d97cfbf078f2dbca0cc3fd87
vctrubio <vctrubio@gmail.com>
2024-08-22 12:39:03 +0200

    debuging production not seeing instagram imgs

--- components/Instagraming.jsx
+++ components/Instagraming.jsx
@@ -20,16 +20,27 @@ const sortInstagramCategory = async () => {
     /* NOTE, THERE IS NO EDGES, ALBUMS ECT*/
     const fields = 'id,caption,media_url,media_type,permalink';  // Replace this with the specific fields you want, e.g., 'id,caption,media_type,media_url'
     const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${process.env.INSTAGRAM_KEY}`;
+    let media = null;
 
-    const data = await fetch(url)
-    console.log("🚀 ~ InstagramFeed ~ data:", data)
-    const media = await data.json();
-    console.log("🚀 ~ InstagramFeed ~ media:", media)
+    try{
+        const res = await fetch(url);
 
+        if (!res.ok) {
+            console.error('Failed to fetch Instagram media:', res.status, res.statusText);
+            const errorResponse = await res.json();
+            console.error('Error details:', errorResponse);
+        }
 
+        media = await res.json();
+    }
+    catch(err){
+        console.error("🚀 ~ InstagramFeed ~ err:", err)
+    }
+    
     const igMedia = []
     const igAlbum = []
     const igVideo = []
+
 
     media.data.forEach(item => {
         if (item.media_type === 'IMAGE') {
@@ -41,15 +52,13 @@ const sortInstagramCategory = async () => {
         }
     })
 
-    // const filteredMedia = media.data.filter(item => item.media_type === 'IMAGE').slice(0, 6);
-    // return filteredMedia
     return {
         igMedia,
         igAlbum,
         igVideo
     }
     // const filteredMedia = media.data.filter(item => item.media_type === 'IMAGE').slice(0, 6);
-
+    // return filteredMedia
 }
 
 
