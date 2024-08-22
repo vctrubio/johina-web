25d6b8ace8d85634b463eec272b58c9e45dcf380
vctrubio <vctrubio@gmail.com>
2024-08-22 12:32:30 +0200

    sort instagram cat

--- components/Instagraming.jsx
+++ components/Instagraming.jsx
@@ -1,4 +1,3 @@
-
 import Image from 'next/image';
 
 export const InstagramCard = ({ media }) => {
@@ -17,21 +16,64 @@ export const InstagramCard = ({ media }) => {
     );
 };
 
-export const InstagramFeed = async () => {
+const sortInstagramCategory = async () => {
     /* NOTE, THERE IS NO EDGES, ALBUMS ECT*/
     const fields = 'id,caption,media_url,media_type,permalink';  // Replace this with the specific fields you want, e.g., 'id,caption,media_type,media_url'
     const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${process.env.INSTAGRAM_KEY}`;
 
     const data = await fetch(url)
+    console.log("🚀 ~ InstagramFeed ~ data:", data)
     const media = await data.json();
+    console.log("🚀 ~ InstagramFeed ~ media:", media)
 
-    const filteredMedia = media.data.filter(item => item.media_type === 'IMAGE').slice(0, 6);
 
+    const igMedia = []
+    const igAlbum = []
+    const igVideo = []
+
+    media.data.forEach(item => {
+        if (item.media_type === 'IMAGE') {
+            igMedia.push(item)
+        } else if (item.media_type === 'CAROUSEL_ALBUM') {
+            igAlbum.push(item)
+        } else if (item.media_type === 'VIDEO') {
+            igVideo.push(item)
+        }
+    })
+
+    // const filteredMedia = media.data.filter(item => item.media_type === 'IMAGE').slice(0, 6);
+    // return filteredMedia
+    return {
+        igMedia,
+        igAlbum,
+        igVideo
+    }
+    // const filteredMedia = media.data.filter(item => item.media_type === 'IMAGE').slice(0, 6);
+
+}
+
+
+
+
+export const InstagramFeed = async () => {
+
+    const {igMedia}  = await sortInstagramCategory();
     return (
         <div className='w-full'>
-            {filteredMedia.map((media, index) => (
+            {igMedia.map((media, index) => (
                 <InstagramCard key={index} media={media} />
             ))}
         </div>
     )
 }
+
+
+/*
+Intagram media_type
+- CAROUSEL_ALBUM
+- VIDEO
+- IMAGE
+
+
+
+*/
